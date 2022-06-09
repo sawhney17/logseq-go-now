566d3a046beead9942e638e20850948d01de2cde
sawhney17 <80150109+sawhney17@users.noreply.github.com>
2022-06-09 08:18:53 +0400

    update for first version

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 import '@logseq/libs';
 import 'logseq-dateutils'
 import { getDateForPageWithoutBrackets } from 'logseq-dateutils';
-const main = async () => {
 
+const settings = [{
+  key: 'keyboardShortcut',
+  title: "Keyboard shortcut",
+  description: 'Keyboard shortcut to trigger going home',
+  type: 'string',
+  default: 'mod+g',
+}]
+const main = async () => {
+  logseq.useSettingsSchema(settings);
   logseq.App.registerCommandPalette({
     key: 'Go home now!',
     label: 'Go to todays journal, append a new block and scroll to it',
-    keybinding: {binding: "mod+g"},
+    keybinding: { binding: "mod+g" },
   }, async () => {
     const dateFormat = (await logseq.App.getUserConfigs()).preferredDateFormat
     const date = getDateForPageWithoutBrackets(new Date(), dateFormat);
