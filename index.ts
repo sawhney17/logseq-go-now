a81c491161a73bd19af343635f9cc25f5d01c02c
sawhney17 <80150109+sawhney17@users.noreply.github.com>
2022-06-21 19:11:39 +0400

    use last blokc if empty

--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
 import '@logseq/libs';
+import { BlockEntity, SettingSchemaDesc } from '@logseq/libs/dist/LSPlugin.user';
 import 'logseq-dateutils'
 import { getDateForPageWithoutBrackets } from 'logseq-dateutils';
 
-const settings = [{
+const settings: SettingSchemaDesc[] = [{
   key: 'keyboardShortcut',
   title: "Keyboard shortcut",
   description: 'Keyboard shortcut to trigger going home',
@@ -14,12 +15,21 @@ const main = async () => {
   logseq.App.registerCommandPalette({
     key: 'Go home now!',
     label: 'Go to todays journal, append a new block and scroll to it',
-    keybinding: { binding: logseq.settings.keyboardShortcut },
+    keybinding: { binding: logseq.settings?.keyboardShortcut },
   }, async () => {
     const dateFormat = (await logseq.App.getUserConfigs()).preferredDateFormat
     const date = getDateForPageWithoutBrackets(new Date(), dateFormat);
-    const block = await logseq.Editor.appendBlockInPage(date, "");
-    logseq.Editor.scrollToBlockInPage(date, block.uuid);
+    const homepage: BlockEntity[] = (await logseq.Editor.getPageBlocksTree(date))
+    const lastItem: BlockEntity = homepage[homepage.length - 1]
+    if (lastItem.content == '') {
+      
+      logseq.Editor.scrollToBlockInPage(date, lastItem.uuid);
+    }
+    else {
+      const block = await logseq.Editor.appendBlockInPage(date, "");
+      logseq.Editor.scrollToBlockInPage(date, block.uuid);
+
+    }
   }
   )
 
